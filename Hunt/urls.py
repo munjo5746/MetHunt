3573b64d781f582d43ed94785f35a05e0266eab4
Edward <munjo5746@gmail.com>
2015-07-29 21:39:20 -0400

    submitting right asnwer will redirect to the next questions

--- Hunt/urls.py
+++ Hunt/urls.py
@@ -5,5 +5,5 @@ from Hunt import views
 urlpatterns = [
     url(r"^HuntMain/$", views.Main, name="HuntMain"),
     url(r"HuntBegin/(?P<HuntPk>[0-9]+)/$", views.HuntBegin, name="HuntBegin"),
-    url(r"HuntDetail/([0-9]+)/([0-9]+)/$", views.HuntDetail, name="HuntDetail"),
+    url(r"HuntDetail/(\d+)/(-?\d+)/$", views.HuntDetail, name="HuntDetail"),
 ]
--- Hunt/views.py
+++ Hunt/views.py
@@ -18,7 +18,8 @@ def HuntBegin(request, HuntPk):
     """
     This function will be called when the user clicks the Hunt they want to
     try out, and they will see the first Hunt page with starting Location
-    in the Museum.
+    in the Museum. At the beginning, the url arguments will be pass with
+    HuntPk and -1 where the -1 indicate that it has been just started.
     """
     data = {}
     page = "HuntBegin.html"
@@ -37,7 +38,7 @@ def HuntBegin(request, HuntPk):
     print serialized
     data.update({'Hunt' : serialized.data})
     data.update({'CurrentIndex' : 0})
-    data.update({'url' : "/Hunt/HuntDetail/" + str(HuntPk) + "/" + str(0)})
+    data.update({'url' : "/Hunt/HuntDetail/" + str(HuntPk) + "/" + str(-1)})
 
     return render_to_response(page, data)
 
@@ -55,14 +56,29 @@ def HuntDetail(request, HuntPk, CurrentIndex):
     CurrentIndex = int(CurrentIndex)
 
     data = {}
-    page = "HuntDetail.html"
+    page = None
     hunt = None
+    items = None
     item = None
     try:
         hunt = Hunt.objects.get(pk=HuntPk)
         items = json.loads(hunt.Items)
     except:
         raise Http404
+
+    # block if CurrentIndex is -1
+    if CurrentIndex == -1:
+        try:
+            item = Item.objects.get(pk=items[CurrentIndex + 1]) # at 0 index
+        except:
+            raise Http404
+        page = "HuntDetail.html"
+        data.update({"user" : request.user})
+        data.update({"Item" : item})
+        data.update({"Title" : str(CurrentIndex + 1) + "th Question"})
+        data.update({"url" : "/Hunt/HuntDetail/" + str(HuntPk) + "/" + str(CurrentIndex + 1)})
+        data.update({"error" : None})
+        return render_to_response(page, data)
 
     # get the size of the item list
     listSize = len(items)
@@ -74,26 +90,43 @@ def HuntDetail(request, HuntPk, CurrentIndex):
     # get the previous item.
     prevItem = Item.objects.get(pk=items[CurrentIndex])
 
+    # get the submitted answer.
+    Answer = request.GET.get("Answer")
+
+
     # compare the answer.
-    if request.method == "GET":
-        Answer = request.GET.get("Answer")
-        print Answer
-        if Answer == prevItem.QuestionId:
+    if request.method == "GET" and Answer is not None:
+
+        if str(Answer) == prevItem.QuestionId:
             # the user got the answer..
             print "The answer is right, answer : " + str(Answer)
-            return render_to_response("HuntBegin.html", {})
+            # The CurrentIndex is not the last one. That means there is more
+            # items to populate. Also, at this moment, the answer is correct.
+            # It means we need to populate next Item.
+
+            # get the item
+            item = Item.objects.get(pk=items[CurrentIndex + 1]) # the next item
+
+            # set the data we need
+            page = "HuntDetail.html"
+            data.update({"user" : request.user})
+            data.update({"Item" : item})
+            data.update({"Title" : str(CurrentIndex + 1) + "th Question"})
+            data.update({"url" : "/Hunt/HuntDetail/" + str(HuntPk) + "/" + str(CurrentIndex + 1)})
+            return render_to_response(page, data)
         else:
             # the submitted answer is wrong..
+            # show that the submitted answer is wrong...
+            page = "HuntDetail.html"
+            data.update({"user" : request.user})
+            data.update({"Title" : "Incorrect Answer"})
+            data.update({"url" : "/Hunt/HuntDetail/" + str(HuntPk) + "/" + str(CurrentIndex)})
+            data.update({"error" : "The submmitted answer is not correct!"})
             print "The answer is wrong, answer : " + str(Answer)
-            return render_to_response("HuntBegin.html", {})
-    else:
-        print "It is not get method!"
-        return render_to_response("HuntBegin.html", {})
+            return render_to_response(page, data)
 
-    # get the item
-    item = Item.objects.get(pk=items[CurrentIndex + 1]) # the next item
-    data.update({"Item" : item})
-    data.update({"Title" : str(CurrentIndex + 1) + "th Question"})
-    data.update({"url" : "/Hunt/HuntDetail/" + str(HuntPk) + "/" + str(CurrentIndex + 1)})
+
+
+
 
     return render_to_response(page, data)
