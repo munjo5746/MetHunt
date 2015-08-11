17cd9833823014176612b37d1cfe876f64d04494
Edward <munjo5746@gmail.com>
2015-08-11 16:47:07 -0400

    basic test case added

--- Hunt/tests.py
+++ Hunt/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+# import modules
+from Hunt.views import Main
 
+
+from django.test import TestCase
+import unittest2 as utest
+import mock
 # Create your tests here.
+
+
+class HuntTest(utest.TestCase):
+
+    def setUp(self):
+        self.request = mock.MagicMock()
+
+    @mock.patch('Hunt.views.render_to_response')
+    def test_HuntMain(self, render):
+        Main(r)
+        render.assert_called_with()
